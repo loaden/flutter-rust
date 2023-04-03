5c88a3af9997ed1a5e4ef3fb0e6d0365e895ef52
Yuchen Deng <loaden@gmail.com>
2023-04-03 09:05:27 +0800

    add string_borrow

--- rust/e01-slice/src/main.rs
+++ rust/e01-slice/src/main.rs
@@ -1,14 +1,22 @@
 fn main() {
     let mut s = String::from("hello");
 
-    println!("fn main: {}", mut_string_borrow(&mut s));
-    // let r = mut_string_param(&mut s);
-    s.clear();
-    // println!("fn main: {}", r);
+    // println!("fn main: {}", mut_string_borrow(&mut s));
+    let r = mut_string_borrow(&mut s);
+    println!("fn main: {}", r);
+
+    let r = string_borrow(&s);
+    // s.clear();
+    println!("fn main: {}", r);
 }
 
 fn mut_string_borrow(s: &mut String) -> &str {
     *s += " world";
     println!("fn mut_string_borrow: {}", s);
     &s[1..]
+}
+
+fn string_borrow(s: &String) -> &str {
+    println!("fn string_borrow: {}", *s);
+    &s[2..]
 }
