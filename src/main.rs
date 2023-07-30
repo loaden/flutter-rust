ef9590162e2531acf1e60c611fd25e4eab95738b
Yuchen Deng <loaden@gmail.com>
2023-07-30 22:06:21 +0800

    竟然支持中文变量名

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,8 @@ fn main() {
     let mut s = String::new();
     println!("请输入一个字符串：");
     io::stdin().read_line(&mut s).expect("failed");
+    s += "test";
     println!("输出：{}", s.to_uppercase());
+    let 中文变量 = String::from("我操");
+    println!("{}", 中文变量);
 }
