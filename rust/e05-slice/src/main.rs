04d6072455bc47b74cbd7ac21029e57dba02fdcd
Yuchen Deng <loaden@gmail.com>
2023-07-30 19:11:40 +0800

    完成切片示例

--- rust/e05-slice/src/main.rs
+++ rust/e05-slice/src/main.rs
@@ -1,22 +1,30 @@
 fn main() {
     let mut s = String::from("hello");
+    println!("{}", &s[1..]);
+    println!("{}", test_slice(&s));
 
-    // println!("fn main: {}", mut_string_borrow(&mut s));
-    let r = mut_string_borrow(&mut s);
-    println!("fn main: {}", r);
-
-    let r = string_borrow(&s);
     // s.clear();
-    println!("fn main: {}", r);
+    let r = string_borrow(&s);
+    println!("main: {}", r);
+
+    let r = string_borrow_mut(&mut s);
+    // s.clear();
+    println!("main: {}", r);
+
+    s.clear();
 }
 
-fn mut_string_borrow(s: &mut String) -> &str {
+fn string_borrow_mut(s: &mut String) -> &str {
     *s += " world";
-    println!("fn mut_string_borrow: {}", s);
+    println!("string_borrow_mut: {}", s);
     &s[1..]
 }
 
 fn string_borrow(s: &String) -> &str {
-    println!("fn string_borrow: {}", *s);
-    &s[2..]
+    println!("string_borrow: {}", *s);
+    &s[2..4]
+}
+
+fn test_slice(s: &String) -> String {
+    s[..3].to_string()
 }
