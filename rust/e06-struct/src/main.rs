170fb58504ac9810e11a0f070ef00854d6c62199
Yuchen Deng <loaden@gmail.com>
2023-07-30 21:34:31 +0800

    优化结体体调试输出

--- rust/e06-struct/src/main.rs
+++ rust/e06-struct/src/main.rs
@@ -5,8 +5,21 @@ fn main() {
         age: 9,
     };
     println!("{}, {}, {}", u.username, u.email, u.age);
+    let u2 = User {
+        username: String::from("Name2"),
+        email: u.email,
+        age: 10,
+    };
+    println!("{:#?}", u2);
+
+    // 元组定义风格
+    #[derive(Debug)]
+    struct Point(i32, i32, f64);
+    let p = Point(12, 12, 15.8);
+    println!("{:?}", p);
 }
 
+#[derive(Debug)]
 struct User {
     username: String,
     email: String,
