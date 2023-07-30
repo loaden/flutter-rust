1b3c94644d9596908609d2fdb6e5e4eb64b32c73
Yuchen Deng <loaden@gmail.com>
2023-07-30 19:32:46 +0800

    添加整数切片

--- rust/e05-slice/src/main.rs
+++ rust/e05-slice/src/main.rs
@@ -4,27 +4,40 @@ fn main() {
     println!("{}", test_slice(&s));
 
     // s.clear();
-    let r = string_borrow(&s);
+    let r = string_slice(&s);
     println!("main: {}", r);
 
-    let r = string_borrow_mut(&mut s);
+    let r = string_slice_mut(&mut s);
     // s.clear();
     println!("main: {}", r);
 
     s.clear();
+
+    let i = [1, 2, 3, 4, 5];
+    let a = array_slice(&i);
+    for j in a {
+        println!("main: {}", j);
+    }
 }
 
-fn string_borrow_mut(s: &mut String) -> &str {
+fn string_slice_mut(s: &mut String) -> &str {
     *s += " world";
-    println!("string_borrow_mut: {}", s);
+    println!("string_slice_mut: {}", s);
     &s[1..]
 }
 
-fn string_borrow(s: &String) -> &str {
-    println!("string_borrow: {}", *s);
+fn string_slice(s: &String) -> &str {
+    println!("string_slice: {}", *s);
     &s[2..4]
 }
 
 fn test_slice(s: &String) -> String {
     s[..3].to_string()
+}
+
+fn array_slice(a: &[i32]) -> &[i32] {
+    for i in &a[1..4] {
+        println!("array_slice: {}", i);
+    }
+    &a[..2]
 }
