02b98621ad1505f8934102b0df0d1e40a28a594d
Yuchen Deng <loaden@gmail.com>
2023-07-30 15:10:26 +0800

    从函数中转回所有权

--- rust/e03-move/src/main.rs
+++ rust/e03-move/src/main.rs
@@ -3,11 +3,22 @@ fn main() {
     let s2 = s1;
     println!("{}", takes_ownership(s2));
     // println!("{}", s1);
-    println!("{}", s2);
+    // println!("{}", s2);
+    let s3 = String::from("hello");
+    let mut s4 = takes_ownership_gives_back(s3);
+    s4.push_str("string");
+    // println!("{}", s3);
+    println!("{}", s4);
 }
 
 fn takes_ownership(mut s: String) ->usize {
     s.push('A');
     println!("takes_ownership: {}", s);
     s.capacity()
+}
+
+fn takes_ownership_gives_back(mut s: String) -> String {
+    s.push('B');
+    println!("takes_ownership_gives_back: {}", s);
+    s
 }
