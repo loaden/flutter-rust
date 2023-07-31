df965f0fdf570e541f6a81ead1747c7a9761220c
Yuchen Deng <loaden@gmail.com>
2023-07-31 13:33:03 +0800

    修改默认示例

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
-use std::io;
-
 fn main() {
-    let mut s = String::new();
-    println!("请输入一个字符串：");
-    io::stdin().read_line(&mut s).expect("failed");
-    s += "test";
-    println!("输出：{}", s.to_uppercase());
-    let 中文变量 = String::from("我操");
-    println!("{}", 中文变量);
+    let p = Message::Point { x: 32, y: 66 };
+    p.print();
+    match p {
+        Message::Point { x, y } => {
+            println!("{}, {}", x, y);
+        },
+        _ => (),
+    }
+}
+
+#[derive(Debug)]
+enum Message {
+    Point { x: i32, y: i32 },
+    _Title(String),
+}
+
+impl Message {
+    fn print(&self) {
+        println!("{:#?}", self);
+    }
 }
