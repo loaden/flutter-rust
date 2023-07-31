8cf8b6163966c865d5a47fe12aa29075fb92ed40
Yuchen Deng <loaden@gmail.com>
2023-07-31 13:10:45 +0800

    访问枚举值的方式

--- rust/e07-enum/src/main.rs
+++ rust/e07-enum/src/main.rs
@@ -3,10 +3,25 @@ fn main() {
     let p = Message::Point { x: 32, y: 66 };
     let t = Message::Title(String::from("title"));
     let c = Message::Color(255, 0, 0);
-    print_enum(q);
-    print_enum(p);
-    print_enum(t);
-    print_enum(c);
+    print_enum(&q);
+    print_enum(&p);
+    print_enum(&t);
+    print_enum(&c);
+    q.print();
+    p.print();
+    t.print();
+    c.print();
+
+    match p {
+        Message::Point { x, y } => {
+            println!("Message::Point {}, {}", x, y);
+        },
+        _ => ()
+    }
+
+    if let Message::Point { x: 32, y: 66 } = p {
+        println!("Message::Point {:?}", p);
+    }
 }
 
 #[derive(Debug)]
@@ -17,7 +32,13 @@ enum Message {
     Color(i32, i32, i32)
 }
 
-fn print_enum(m: Message) -> Message {
+fn print_enum(m: &Message) -> &Message {
     println!("{:?}", m);
     m
+}
+
+impl Message {
+    fn print(&self) {
+        println!("{:?}", self);
+    }
 }
