b89ed68db203047301200e6996c01a03a7ec9cf8
Yuchen Deng <loaden@gmail.com>
2023-08-01 14:56:01 +0800

    Vec通过枚举存储不同类型值

--- rust/e10-vector/src/main.rs
+++ rust/e10-vector/src/main.rs
@@ -1,7 +1,37 @@
 fn main() {
-    let v = vec![1, 2, 3, 4, 5];
-    for i in &v  {
+    let mut v = vec![1, 2, 3, 4, 5];
+    for i in &mut v {
+        *i += 1;
         println!("{}", i);
     }
-    println!("{}", v.len());
+    println!("{}", v.first().unwrap());
+
+    let g = v.get(10);
+    match g {
+        Some(i) => println!("{}", i),
+        None => println!("Not exist"),
+    }
+
+    let mut m = vec![Msg::Title(String::from("title")), Msg::Length(12)];
+
+    m.push(Msg::Title(String::from("hello")));
+    m.push(Msg::Length(88));
+
+    for i in &mut m {
+        match i {
+            Msg::Title(x) => println!("String: {}", x),
+            Msg::Length(x) => println!("i32: {}", x),
+        }
+
+        if let Msg::Title(t) = i {
+            println!("if let Title: {}", t);
+        } else if let Msg::Length(t) = i {
+            println!("else if let Length: {}", t);
+        }
+    }
+}
+
+enum Msg {
+    Title(String),
+    Length(i32),
 }
