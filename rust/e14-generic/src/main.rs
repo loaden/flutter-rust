417d00698d33264919666d24cface2b56d55e129
Yuchen Deng <loaden@gmail.com>
2023-08-02 23:35:14 +0800

    为泛型函数添加约束

--- rust/e14-generic/src/main.rs
+++ rust/e14-generic/src/main.rs
@@ -1,22 +1,24 @@
-use std::cmp::PartialOrd;
+use std::{cmp::PartialOrd, fmt::Display};
 
 fn main() {
     let a = 32;
     let b = 35;
-    println!("{}", bigger_test(&a, &b));
-    let p1 = Point { x: 10, y: 20 };
-    let p2 = Point { x: 1.0, y: 3.5 };
+    println!("{}", bigger_test(a, b));
+    let c = String::from("cccc");
+    let d = String::from("dddd");
+    println!("{}", bigger_test(c, d));
+
+    let _p1 = Point { x: 10, y: 20 };
+    let _p2 = Point { x: 1.0, y: 3.5 };
 }
 
-fn bigger_test<T>(x: &T, y: &T) -> T {
+fn bigger_test<T: PartialOrd + Display>(x: T, y: T) -> T {
     if x > y {
         x
     } else {
         y
     }
 }
-
-impl<T> PartialOrd for bigger_test<T> {}
 
 struct Point<T> {
     x: T,
