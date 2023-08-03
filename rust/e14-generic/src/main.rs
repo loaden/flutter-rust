6a3915df486394109433e28fe12cb3e2cc015da0
Yuchen Deng <loaden@gmail.com>
2023-08-03 20:18:12 +0800

    添加引用版本函数

--- rust/e14-generic/src/main.rs
+++ rust/e14-generic/src/main.rs
@@ -6,13 +6,23 @@ fn main() {
     println!("{}", bigger_test(a, b));
     let c = String::from("cccc");
     let d = String::from("dddd");
+    println!("{}", bigger_test2(&c, &d));
     println!("{}", bigger_test(c, d));
 
-    let _p1 = Point { x: 10, y: 20 };
-    let _p2 = Point { x: 1.0, y: 3.5 };
+    let p1 = Point { x: 10, y: 20 };
+    let p2 = Point { x: 1.0, y: 3.5 };
+    println!("{},{},{},{}", p1.x, p1.y, p2.x, p2.y);
 }
 
 fn bigger_test<T: PartialOrd + Display>(x: T, y: T) -> T {
+    if x > y {
+        x
+    } else {
+        y
+    }
+}
+
+fn bigger_test2<'a, T: PartialOrd>(x: &'a T, y: &'a T) -> &'a T {
     if x > y {
         x
     } else {
