a5905b01ffce873a2066fefed1a16ea6bc8d91d0
Yuchen Deng <loaden@gmail.com>
2023-08-04 00:03:28 +0800

    完善生命周期

--- rust/e16-lifetime/src/main.rs
+++ rust/e16-lifetime/src/main.rs
@@ -1,13 +1,21 @@
 fn main() {
     other_test();
-    let a = String::from("abcd");      // ---------+- 'a
-    let b = "xyz";                       // -+- 'b   |
-                                               //  |       |
-    let r = longest(a.as_str(), b); // -|-------|-------+- 'r
-    println!("{}", r);                         //  |       |       |
-}                                              // -+-------+-------+-
+    let a = String::from("abcd");   // ---------+- 'a
+    let b = String::from("xyz");    // -+- 'b   |
+                                            //  |       |
+    let r = longest(&a, &b);  // -|-------|-------+- 'r
+    println!("{}", r);                      //  |       |       |
+}                                           // -+-------+-------+-
 
-fn longest<'c>(x: &'c str, y: &'c str) -> &'c str {
+fn longest<'d>(x: &'d String, y: &'d String) -> &'d String {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn longest2<'c>(x: &'c str, y: &'c str) -> &'c str {
     if x.len() > y.len() {
         x
     } else {
@@ -17,15 +25,15 @@ fn longest<'c>(x: &'c str, y: &'c str) -> &'c str {
 
 fn other_test() {
     let a = String::from("123");
-    let mut r;
+    let r: &str;
     {
-        let b: &str = "4567";
-        r = longest(a.as_str(), b);
+        let b = "4567";
+        r = longest2(a.as_str(), b);
     }
     println!("{}", r);
-    {
-        let b = String::from("4567");
-        r = longest(a.as_str(), b.as_str());
-    }
-    println!("{}", r);
+    // {
+    //     let b = String::from("4567");
+    //     r = longest2(a.as_str(), b.as_str());
+    // }
+    // println!("{}", r);
 }
