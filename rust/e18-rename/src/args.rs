1e25cce025ca5a069f5108e91c66e3ceb58e412b
Yuchen Deng <loaden@gmail.com>
2023-08-08 14:06:53 +0800

    iter重写环境变量获取

--- rust/e18-rename/src/args.rs
+++ rust/e18-rename/src/args.rs
@@ -19,16 +19,26 @@ impl Config {
 }
 
 pub fn process_cmdline() -> Result<Config, &'static str> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 4 {
-        Err("process_cmdline")
-    } else {
-        println!("{:?}", args);
-        let r = Config::new(
-            args[1].clone(),
-            args[2].clone(),
-            args[3].clone()
-        );
-        Ok(r)
-    }
+    let mut args = env::args();
+    args.next();
+
+    let path = match args.next() {
+        Some(s) => s,
+        None => return Err("path"),
+    };
+    let old_str = match args.next() {
+        Some(s) => s,
+        None => return Err("old_str"),
+    };
+    let new_str = match args.next() {
+        Some(s) => s,
+        None => return Err("old_str"),
+    };
+
+    let r = Config::new(
+        path,
+        old_str,
+        new_str,
+    );
+    Ok(r)
 }
