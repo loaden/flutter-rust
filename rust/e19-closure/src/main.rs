16d63d0e27ea45adc0c873e7dbebf33dac462cc2
Yuchen Deng <loaden@gmail.com>
2023-08-08 11:13:00 +0800

    添加自定义iter

--- rust/e19-closure/src/main.rs
+++ rust/e19-closure/src/main.rs
@@ -67,3 +67,36 @@ impl<T> Cacher<T>
         }
     }
 }
+
+struct Counter {
+    count: usize,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> { // 首字母大写的 Self
+        self.count += 1;
+        if self.count < 4 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+#[test]
+fn test_iterator_next() {
+    let mut c = Counter::new();
+    assert_eq!(c.next(), Some(1));
+    assert_eq!(c.next(), Some(2));
+    assert_eq!(c.next(), Some(3));
+    assert_eq!(c.next(), Some(4));
+    assert_eq!(c.next(), Some(5));
+}
