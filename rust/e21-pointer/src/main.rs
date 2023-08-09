b29dcd8830327ac49a5813f4f72b49df62d94ad7
Yuchen Deng <loaden@live.com>
2023-08-09 23:45:10 +0800

    Rc与RefCell混用

--- rust/e21-pointer/src/main.rs
+++ rust/e21-pointer/src/main.rs
@@ -5,8 +5,11 @@ mod rc;
 use crate::rc::rc_pointer_test;
 
 mod refcell;
+mod mix;
+use crate::mix::mixed::test_rc_refcell_pointer;
 
 fn main() {
     box_pointer_test();
     rc_pointer_test();
+    test_rc_refcell_pointer();
 }
---
+++ rust/e21-pointer/src/mix.rs
@@ -0,0 +1,22 @@
+pub mod mixed {
+    use self::List::{Cons, Nil};
+    use std::cell::RefCell;
+    use std::rc::Rc;
+
+    pub fn test_rc_refcell_pointer() {
+        let value = Rc::new(RefCell::new(5));
+        let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+        let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+        let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
+        *value.borrow_mut() += 10;
+        println!("a after = {:?}", a);
+        println!("b after = {:?}", b);
+        println!("c after = {:?}", c);
+    }
+
+    #[derive(Debug)]
+    enum List<T> {
+        Cons(Rc<RefCell<T>>, Rc<List<T>>),
+        Nil,
+    }
+}
