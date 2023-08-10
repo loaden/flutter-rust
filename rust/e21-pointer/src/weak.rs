522654e92976b86dd6341237697ee2d3b70aa9a1
Yuchen Deng <loaden@live.com>
2023-08-10 12:27:57 +0800

    使用Weak消除循环引用

--- rust/e21-pointer/src/weak.rs
+++ rust/e21-pointer/src/weak.rs
@@ -1,11 +1,49 @@
 use std::cell::RefCell;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
 pub fn weak_pointer_test() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        children: RefCell::new(vec![]),
+        parent: RefCell::new(Weak::new()),
+    });
+    println!("leaf parent1: {:?}", leaf.parent.borrow().upgrade());
+
+    {
+        let branch = Rc::new(Node {
+            value: 2,
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+            parent: RefCell::new(Weak::new()),
+        });
+
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        println!("branch children: {:?}", *branch.children.borrow());
+        println!("leaf parent2: {:?}", leaf.parent.borrow().upgrade());
+
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf)
+        );
+        println!(
+            "branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch)
+        );
+    }
+
+    println!("leaf parent3: {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf)
+    );
 }
 
 #[derive(Debug)]
 struct Node {
     value: i32,
     children: RefCell<Vec<Rc<Node>>>,
+    parent: RefCell<Weak<Node>>,
 }
