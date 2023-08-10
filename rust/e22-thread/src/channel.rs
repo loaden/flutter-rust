36cdb1b61a69aa765251ad66443b191130c4e968
Yuchen Deng <loaden@live.com>
2023-08-10 21:01:32 +0800

    遍历接收器

--- rust/e22-thread/src/channel.rs
+++ rust/e22-thread/src/channel.rs
@@ -9,7 +9,17 @@ pub fn thread_channel() {
         let msg = "hi".to_string();
         thread::sleep(Duration::from_secs(1));
         tx.send(msg).unwrap();
+        // println!("send msg: {}", msg);
         thread::sleep(Duration::from_secs(2));
+
+        let vals = vec![1, 2, 3, 4, 5];
+        for v in vals {
+            tx.send(v.to_string()).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+
+        drop(tx);
+        thread::sleep(Duration::from_secs(3));
     });
 
     println!("begin receiving");
@@ -17,6 +27,11 @@ pub fn thread_channel() {
     println!("end receiving");
     println!("Got: {}", msg);
 
+    for received in rx {
+        println!("Got: {}", received);
+    }
+
+    println!("sending done");
     h.join().unwrap();
-    println!("done");
+    println!("main done");
 }
