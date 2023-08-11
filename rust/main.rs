d2f1b86944451001e39cf32adab9d7fe6b7a6427
Yuchen Deng <loaden@live.com>
2023-08-11 18:51:35 +0800

    简化版错误示例

--- rust/main.rs
+++ rust/main.rs
@@ -1,20 +1,41 @@
 fn main() {
-    let p = Message::Point { x: 32, y: 66 };
-    p.print();
-    match p {
-        Message::Point { x, y } => println!("{}, {}", x, y),
-        _ => (),
+    let p = Post::new();
+    p.add_text(" world");
+    println!("{}", p.content());
+}
+
+trait State {
+    fn add_text<'a>(&self, post: &'a mut Post, text: &'a str) {}
+    fn content<'a>(&self, post: &'a Post) -> &'a str;
+}
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::from("hello"),
+        }
+    }
+
+    pub fn add_text(&mut self, text: &str) {
+        self.state.as_mut().unwrap().add_text(self, text);
+    }
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(&self)
     }
 }
 
-#[derive(Debug)]
-enum Message {
-    Point { x: i32, y: i32 },
-    _Title(String),
-}
-
-impl Message {
-    fn print(&self) {
-        println!("{:#?}", self);
+struct Draft {}
+impl State for Draft {
+    fn add_text<'a>(&self, post: &'a mut Post, text: &'a str) {
+        post.content.push_str(text);
+    }
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
     }
 }
