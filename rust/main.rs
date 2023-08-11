bb86f75aa303d57b3e877ef6ee01e58d4994d826
Yuchen Deng <loaden@live.com>
2023-08-11 19:39:57 +0800

    RefCell比较丑陋的实现

--- rust/main.rs
+++ rust/main.rs
@@ -1,41 +1,44 @@
+use std::cell::RefCell;
+use std::cell::Ref;
+
 fn main() {
-    let p = Post::new();
+    let mut p = Post::new();
     p.add_text(" world");
     println!("{}", p.content());
 }
 
 trait State {
-    fn add_text<'a>(&self, post: &'a mut Post, text: &'a str) {}
-    fn content<'a>(&self, post: &'a Post) -> &'a str;
+    fn add_text<'a>(&self, post: &'a Post, text: &'a str) {}
+    fn content<'a>(&self, post: &'a Post) -> String;
 }
 pub struct Post {
     state: Option<Box<dyn State>>,
-    content: String,
+    content: RefCell<String>,
 }
 
 impl Post {
     pub fn new() -> Post {
         Post {
             state: Some(Box::new(Draft {})),
-            content: String::from("hello"),
+            content: RefCell::new(String::from("hello")),
         }
     }
 
     pub fn add_text(&mut self, text: &str) {
-        self.state.as_mut().unwrap().add_text(self, text);
+        self.state.as_ref().unwrap().add_text(self, text);
     }
 
-    pub fn content(&self) -> &str {
+    pub fn content(&self) -> String {
         self.state.as_ref().unwrap().content(&self)
     }
 }
 
 struct Draft {}
 impl State for Draft {
-    fn add_text<'a>(&self, post: &'a mut Post, text: &'a str) {
-        post.content.push_str(text);
+    fn add_text<'a>(&self, post: &'a Post, text: &'a str) {
+        post.content.borrow_mut().push_str(text);
     }
-    fn content<'a>(&self, post: &'a Post) -> &'a str {
-        &post.content
+    fn content<'a>(&self, post: &'a Post) -> String {
+        (&post.content.borrow()).to_string()
     }
 }
