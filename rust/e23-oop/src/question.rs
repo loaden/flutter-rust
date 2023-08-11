f0e4d742c0e37ce70de6324c466204c4e95c1387
Yuchen Deng <loaden@live.com>
2023-08-11 20:40:43 +0800

    返回Ref<'_, String>

--- rust/e23-oop/src/question.rs
+++ rust/e23-oop/src/question.rs
@@ -1,15 +1,16 @@
 use std::cell::RefCell;
+use std::cell::Ref;
 pub fn test_return_str() {
     let d = Data { data: RefCell::new(String::from("hello")) };
-    println!("{}", d.get1());
-    println!("{}", d.get2());
+    println!("{}, {}", d.get1(), d.get2());
+    let r = d.get2();
 }
 struct Data { data: RefCell<String>, }
 impl Data {
     fn get1(&self) -> String {
         self.data.borrow().to_string()
     }
-    fn get2(&self) -> &String {
-        &self.data.borrow().to_string()
+    fn get2(&self) -> Ref<'_, String> {
+        self.data.borrow()
     }
 }
