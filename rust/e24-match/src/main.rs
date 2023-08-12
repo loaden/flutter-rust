7885cf9b88ebca70e1e06a478660d452a7301dd2
Yuchen Deng <loaden@live.com>
2023-08-12 10:00:02 +0800

    添加结构体和枚举模式匹配

--- rust/e24-match/src/main.rs
+++ rust/e24-match/src/main.rs
@@ -29,4 +29,44 @@ fn main() {
 
     let (x, ..) = (7, 8, 9, 10);
     println!("x = {}, y = {}, z = {}", x, y, z);
+
+    let mut tx = 5;
+    let ty = &mut tx;
+    *ty += 2;
+    tx += 3;
+
+    match tx {
+        1..=10 => println!("{} is between 1 and 10", tx),
+        _ => (),
+    }
+
+    let p = Point { x: 0, y: 20 };
+    let Point { x, y } = p;
+    let Point { x: a, y: b } = p;
+    // let Point { a, b } = p;
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("At ({}, {})", x, y),
+    }
+
+    let msg = Message::Quit;
+    match msg {
+        Message::ChangeColor(r, g, b) => println!("{},{},{}", r, g ,b),
+        Message::Move { x, y } => println!("xy:{},{}", x, y),
+        Message::Write(s) => println!("s = {}", s),
+        Message::Quit => println!("Quit"),
+    }
+}
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
