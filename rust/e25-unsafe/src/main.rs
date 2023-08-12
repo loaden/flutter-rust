3e1491975c88cb354d1a99ddc7653931ae7844da
Yuchen Deng <loaden@live.com>
2023-08-12 19:04:43 +0800

    修改泛型默参

--- rust/e25-unsafe/src/main.rs
+++ rust/e25-unsafe/src/main.rs
@@ -2,7 +2,11 @@ mod raw_pointer;
 use raw_pointer::raw_pointer_test;
 
 mod state;
-use state::Post;
+use crate::state::Post;
+
+mod r#trait;
+use crate::r#trait::Point;
+use crate::r#trait::{Millimeters, Meters};
 
 fn main() {
     let mut p = Post::new();
@@ -10,4 +14,14 @@ fn main() {
     println!("{}", p.content());
 
     raw_pointer_test();
+
+    let p1 = Point { x: 1, y: 0 };
+    let p2 = Point { x: 2, y: 3 };
+    let p3 = p1 + p2;
+    println!("{:?}", p3);
+
+    let mm = Millimeters(1);
+    let m = Meters(1);
+    let nm = mm + m;
+    println!("{}", nm.0);
 }
---
+++ rust/e25-unsafe/src/trait.rs
@@ -0,0 +1,28 @@
+use std::ops::Add;
+
+#[derive(Debug, PartialEq)]
+pub struct Point {
+    pub x: i32,
+    pub y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+pub struct Millimeters(pub u32);
+pub struct Meters(pub u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + other.0 * 1000)
+    }
+}
