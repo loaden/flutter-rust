8e6b5f4fa75fd6c8670992aa270e5de3fa64a39d
Yuchen Deng <loaden@live.com>
2023-08-13 21:44:06 +0800

    学习编写宏

--- rust/e27-macro/src/main.rs
+++ rust/e27-macro/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let v = ve![1, 2];
+    println!("{:?}", v);
+    let v = ve!(3, 4);
+    println!("{:?}", v);
+}
+
+#[macro_export]
+macro_rules! ve {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
 }
