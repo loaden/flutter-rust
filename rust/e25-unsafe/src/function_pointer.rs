7e8d55c059ae6815419e723c1f1d766df4f1920a
Yuchen Deng <loaden@live.com>
2023-08-13 21:10:34 +0800

    返回闭包

--- rust/e25-unsafe/src/function_pointer.rs
+++ rust/e25-unsafe/src/function_pointer.rs
@@ -20,6 +20,13 @@ pub fn function_pointer() {
     let list_of_statuses: Vec<Status> = (0_u32..20)
         .map(|i| Status::Value(i)).collect();
     println!("闭包风格 {:?}", list_of_statuses);
+
+    let f = return_closure();
+    println!("return closure: {}", (*f)(1));
+}
+
+fn return_closure() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| x + 1 )
 }
 
 #[derive(Debug)]
