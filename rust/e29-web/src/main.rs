6c0eabff1102b0668eab31d4de7349cc4f94faa3
Yuchen Deng <loaden@live.com>
2023-08-14 22:32:50 +0800

    只允许根目录访问

--- rust/e29-web/src/main.rs
+++ rust/e29-web/src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     env, fs,
-    io::prelude::*,
+    io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
 
@@ -18,10 +18,15 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("index.html").unwrap();
-    let length = contents.len();
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    println!("{}", response);
-    stream.write_all(response.as_bytes()).unwrap();
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("index.html").unwrap();
+        let length = contents.len();
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        println!("{}", response);
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
