e566ef64c166ded90c0390edfc81555cb91497e5
Yuchen Deng <loaden@live.com>
2023-08-14 15:10:40 +0800

    添加自定义derive示例

--- rust/e27-macro/src/main.rs
+++ rust/e27-macro/src/main.rs
@@ -1,8 +1,16 @@
+use hello_macro::HelloMacro;
+use hello_macro_derive::HelloMacro;
+
+#[derive(HelloMacro)]
+struct TestDerive;
+
 fn main() {
     let v = ve![1, 2];
     println!("{:?}", v);
     let v = ve!(3, 4);
     println!("{:?}", v);
+
+    TestDerive::hello_macro();
 }
 
 #[macro_export]
