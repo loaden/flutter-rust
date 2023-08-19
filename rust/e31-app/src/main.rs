4d96939e7746541b5c6fec6c38e6f86d95a11e50
Yuchen Deng <loaden@live.com>
2023-08-20 00:17:06 +0800

    实现view

--- rust/e31-app/src/main.rs
+++ rust/e31-app/src/main.rs
@@ -1,5 +1,6 @@
 use iced::executor;
-use iced::{Application, Command, Element, Settings, Theme};
+use iced::widget::{button, column, container};
+use iced::{Application, Command, Element, Settings, Theme, Alignment, Length};
 use iced::window;
 
 fn main() -> iced::Result {
@@ -18,13 +19,19 @@ struct App {
     exit_confirm: bool,
 }
 
+#[derive(Debug, Clone)]
+enum Message {
+    Confirm,
+    Exit,
+}
+
 impl Application for App {
     type Executor = executor::Default;
-    type Message = ();
+    type Message = Message;
     type Theme = Theme;
     type Flags = ();
 
-    fn new(_flags: ()) -> (Self, Command<Self::Message>) {
+    fn new(_flags: ()) -> (Self, Command<Message>) {
         (Self::default(), Command::none())
     }
 
@@ -32,11 +39,35 @@ impl Application for App {
         String::from("App - Iced")
     }
 
-    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+    fn update(&mut self, _message: Message) -> Command<Message> {
         Command::none()
     }
 
-    fn view(&self) -> Element<Self::Message> {
-        "Hello World!".into()
+    fn view(&self) -> Element<Message> {
+        let content = if self.exit_confirm {
+            column![
+                "Are you sure you want to exit?",
+                button("Yes, exit now")
+                    .padding([10, 20])
+                    .on_press(Message::Confirm),
+            ]
+        } else {
+            column![
+                "Click the button to exit",
+                button("Exit")
+                    .padding([10, 20])
+                    .on_press(Message::Exit),
+            ]
+        }
+        .spacing(10)
+        .align_items(Alignment::Center);
+
+        container(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .padding(20)
+            .center_x()
+            .center_y()
+            .into()
     }
 }
