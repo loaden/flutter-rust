5b1f29ce9bed97dd84c75a08e7663460315d7ffc
Yuchen Deng <loaden@live.com>
2023-08-21 19:52:11 +0800

    添加复选框

--- rust/e34-state/src/main.rs
+++ rust/e34-state/src/main.rs
@@ -1,30 +1,46 @@
-use iced::{Element, Sandbox, Settings};
+use iced::widget::{checkbox, column, container, text};
+use iced::{Element, Length, Sandbox, Settings};
 
 pub fn main() -> iced::Result {
     Box::run(Settings::default())
 }
 
-struct Box;
+struct Box {
+    debug: bool,
+}
 
 #[derive(Debug)]
-enum Message {}
+enum Message {
+    NextPage,
+    PrevPage,
+    DebugMode(bool),
+}
 
 impl Sandbox for Box {
     type Message = Message;
 
     fn new() -> Self {
-        Self
+        Self { debug: false }
     }
 
     fn title(&self) -> String {
         String::from("State - Iced")
     }
 
-    fn update(&mut self, _message: Message) {
-        todo!()
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::DebugMode(b) => self.debug = b,
+            _ => (),
+        }
     }
 
     fn view(&self) -> Element<Message> {
-        "".into()
+        let content = column![checkbox("Debug mode", self.debug, Message::DebugMode),];
+        container(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_x()
+            .center_y()
+            .into()
     }
 }
