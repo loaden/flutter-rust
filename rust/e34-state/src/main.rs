53bfd3903054f7d3cf983de21567daf514194edc
Yuchen Deng <loaden@live.com>
2023-08-21 20:39:55 +0800

    实现最基本的页面跳转

--- rust/e34-state/src/main.rs
+++ rust/e34-state/src/main.rs
@@ -7,20 +7,31 @@ pub fn main() -> iced::Result {
 
 struct Box {
     debug: bool,
+    page: Page,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 enum Message {
     NextPage,
     PrevPage,
     DebugMode(bool),
 }
 
+#[derive(PartialEq)]
+enum Page {
+    Welcome,
+    CheckBox,
+    End,
+}
+
 impl Sandbox for Box {
     type Message = Message;
 
     fn new() -> Self {
-        Self { debug: false }
+        Self {
+            debug: false,
+            page: Page::Welcome,
+        }
     }
 
     fn title(&self) -> String {
@@ -30,22 +41,31 @@ impl Sandbox for Box {
     fn update(&mut self, message: Message) {
         match message {
             Message::DebugMode(b) => self.debug = b,
-            _ => (),
+            Message::NextPage => self.page = Page::CheckBox,
+            Message::PrevPage => self.page = Page::Welcome,
         }
     }
 
     fn view(&self) -> Element<Message> {
-        let content = column![
-            checkbox("Debug mode", self.debug, Message::DebugMode),
-            row![
-                button("Prev").on_press(Message::PrevPage),
-                button("Next").on_press(Message::NextPage),
-            ]
-            .spacing(10)
-        ]
-        .spacing(20);
+        let mut content = match self.page {
+            Page::Welcome => column!["Welcome!"],
+            Page::CheckBox => column![checkbox("Debug mode", self.debug, Message::DebugMode),],
+            Page::End => column!["End"],
+        };
 
-        container(content)
+        content = match self.page {
+            Page::Welcome => content.push(button("Next").on_press(Message::NextPage)),
+            Page::End => content.push(button("Prev").on_press(Message::PrevPage)),
+            _ => content.push(
+                row![
+                    button("Prev").on_press(Message::PrevPage),
+                    button("Next").on_press(Message::NextPage),
+                ]
+                .spacing(10),
+            ),
+        };
+
+        container(content.spacing(20))
             .width(Length::Fill)
             .height(Length::Fill)
             .center_x()
