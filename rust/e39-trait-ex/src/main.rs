569659002b0d222580224df5b1f68b6e08d7bf2e
Yuchen Deng <loaden@live.com>
2023-08-28 23:06:54 +0800

    携带数据的枚举会自动实现Fn(数据) -> 枚举类型

---
+++ rust/e39-trait-ex/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    slider(0..=10, 5, StepMessage::SliderChanged);
+    slider(0..=10, 5, |_| StepMessage::SliderChanged(8));
+}
+
+#[derive(Debug, Clone)]
+enum StepMessage {
+    SliderChanged(u8),
+}
+
+pub fn slider<'a, T, Message>(
+    range: std::ops::RangeInclusive<T>,
+    value: T,
+    on_change: impl Fn(T) -> Message + 'a,
+) where
+    T: Copy + From<u8> + std::cmp::PartialOrd,
+    Message: Clone,
+{
+}
