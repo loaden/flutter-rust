f3fae6bf1c2f5524b2bf7bbb3ccbae9f4d12f926
loaden <loaden@live.com>
2023-09-24 19:54:39 +0800

    操作路径

--- rust/e41-command/src/main.rs
+++ rust/e41-command/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::Read;
 use std::process::Command;
 use std::process::Stdio;
+use std::path::PathBuf;
 
 fn main() {
     if let Ok(mut child) = Command::new("clang")
@@ -10,9 +11,16 @@ fn main() {
         .spawn()
     {
         let mut stderr = child.stderr.take().unwrap();
-        let mut str = String::from("");
+        let mut str = String::new();
         let _ = stderr.read_to_string(&mut str);
         println!("ERROR: {:#?}", str);
+        let v: Vec<_> = str.split("InstalledDir: ").collect();
+        println!("Target: {:#?}", v[1].trim_end());
+        let mut path = PathBuf::from(v[1].trim_end());
+        path.pop();
+        if let Some(s) = path.to_str() {
+            println!("DONE: {:?}", s);
+        }
 
         let output = child.wait_with_output().expect("failed to wait on child");
         if output.status.success() {
