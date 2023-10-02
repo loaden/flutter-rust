1cd86b7c14b56c36636e51044270379f211faf12
loaden <loaden@live.com>
2023-10-02 14:30:41 +0800

    重写Result学习示例

--- rust/e13-result/src/main.rs
+++ rust/e13-result/src/main.rs
@@ -1,26 +1,33 @@
-use std::{fs::File, io::ErrorKind};
-use std::io;
-use std::io::Read;
-use std::fs;
+use std::fs::{self, File, OpenOptions};
+use std::io::{self, Read, Write};
 
-fn main() {
+fn main() -> io::Result<()> {
     let filename = "test.txt";
-    let _f = match File::open(filename) {
+    let mut file = match OpenOptions::new()
+        .append(true)
+        .read(true)
+        .create(true)
+        .open(filename)
+    {
         Ok(file) => file,
-        Err(err) => match err.kind() {
-            ErrorKind::NotFound => match File::create(filename) {
-                Ok(file) => file,
-                Err(err) => panic!("create file failed: {:?}", err),
-            },
-            ErrorKind::Other => panic!("what happened"),
-            _ => panic!("??"),
-        },
+        Err(_err) => panic!("what happened"),
     };
 
-    let _f = File::open(filename).unwrap();
-    let _f = File::open(filename).expect("msg");
+    file.write_all(b"test").unwrap();
+    file.flush().unwrap();
 
-    match read_file() {
+    let mut buf = String::new();
+    match file.read_to_string(&mut buf) {
+        Ok(n) => println!("f.read_to_string: {}, {}", buf, n),
+        Err(e) => println!("{}", e),
+    };
+
+    match fs::read_to_string(filename) {
+        Ok(s) => println!("read: {}", s),
+        Err(e) => println!("read error: {}", e),
+    }
+
+    match test_read_file() {
         Ok(s) => println!("{}", s),
         Err(e) => println!("Err: {}", e),
     }
@@ -30,15 +37,11 @@ fn main() {
         Err(e) => println!("Err: {}", e),
     }
 
-    match fs::read_to_string(filename) {
-        Ok(s) => println!("read: {}", s),
-        Err(e) => println!("read error: {}", e),
-    }
-
-    let _ = fs::remove_file(filename);
+    fs::remove_file(filename)?;
+    Ok(())
 }
 
-fn read_file() -> Result<String, io::Error> {
+fn test_read_file() -> Result<String, io::Error> {
     let mut f = File::open("non-exist")?;
     let mut s = String::new();
     f.read_to_string(&mut s)?;
