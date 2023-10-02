930141f73caae4ee84a306b8eb37a30d2ec01ed1
loaden <loaden@live.com>
2023-10-02 14:30:21 +0800

    读写文件测试

--- rust/main.rs
+++ rust/main.rs
@@ -1,13 +1,22 @@
-struct Config {}
-fn main() {
-    let _cfg = test().unwrap_or_else(|err| {
-        println!("{}", err);
-        never()
-    });
-}
-fn test() -> Result<Config, &'static str> {
-    Err("error")
-}
-fn never() -> ! {
-    panic!("panic")
+use std::fs::OpenOptions;
+use std::io::{self, Read, Write};
+
+fn main() -> io::Result<()> {
+    let filename = "test.txt";
+    let mut file = OpenOptions::new()
+        .append(true)
+        .read(true)
+        .create(true)
+        .open(filename)?;
+
+    file.write_all(b"test")?;
+    file.flush()?;
+
+    let mut buf = String::new();
+    match file.read_to_string(&mut buf) {
+        Ok(n) => println!("f.read_to_string: {}, {}", buf, n),
+        Err(e) => println!("{}", e),
+    };
+
+    Ok(())
 }
