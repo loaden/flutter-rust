5c230b514accec21227c10ac38b59507de0706f4
loaden <loaden@live.com>
2023-10-14 20:40:26 +0800

    可写的OnceLock

--- rust/e43-global/src/main.rs
+++ rust/e43-global/src/main.rs
@@ -1,5 +1,8 @@
 use std::{sync::OnceLock, thread};
 
+static APP: OnceLock<App> = OnceLock::new();
+static mut APP2: OnceLock<App2> = OnceLock::new();
+
 fn main() {
     // 子线程中调用
     let handle = thread::spawn(|| {
@@ -10,32 +13,55 @@ fn main() {
     // 主线程调用
     let logger = App::global();
     logger.log("some message".to_string());
+    // logger.save("message".to_string());
 
-    let logger2 = App::global();
-    logger2.log("other message".to_string());
+    App2::global();
+    unsafe {
+        let logger2 = APP2.get_mut().unwrap();
+        logger2.save("mut_".to_string());
+        logger2.save("mut_".to_string());
+    }
 
     handle.join().unwrap();
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct App {
     name: String,
 }
-
-static APP: OnceLock<App> = OnceLock::new();
 
 impl App {
     fn global() -> &'static App {
         // 获取或初始化
         APP.get_or_init(|| {
             println!("App is being created..."); // 初始化打印
-            App {
-                name: String::from("global"),
-            }
+            App::default()
         })
     }
 
     fn log(&self, message: String) {
         println!("{}: {}", self.name, message);
+    }
+}
+
+#[derive(Debug, Default)]
+struct App2 {
+    msgs: String,
+}
+
+impl App2 {
+    fn global() -> &'static App2 {
+        unsafe {
+            // 获取或初始化
+            APP2.get_or_init(|| {
+                println!("App2 is being created..."); // 初始化打印
+                App2::default()
+            })
+        }
+    }
+
+    fn save(&mut self, message: String) {
+        self.msgs += message.as_str();
+        println!("self.msgs: {}", self.msgs);
     }
 }
